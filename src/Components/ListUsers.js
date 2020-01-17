5d908dbb01bdfecc173e763fe849e085f9347f87
Omar <omarcr.96@gmail.com>
2020-01-17 00:30:42 -0600

    Se añadieron la propiedades keys al mapeado de las CardUser

--- src/Components/ListUsers.js
+++ src/Components/ListUsers.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import CardUser from './CardUser'
+import React from "react";
+import CardUser from "./CardUser";
 
-function ListUsers (props){
-  return(
+function ListUsers(props) {
+  return (
     <div>
-      {
-        props.items.map((person) => {
-          return(
-            <CardUser
-              {...person}
-            />
-          )
-        })
-      }
+      {props.items.map((person, index) => {
+        return (
+          <div key={index}>
+            <CardUser {...person} />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 export default ListUsers;
